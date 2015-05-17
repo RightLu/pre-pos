b4a49a45c16c84633bf8e6e7efe57ffdf6df7e1a
renlu <rightlu@outlook.com>
2015-05-17 23:25:42 +0800

    add sixth pre-pos

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -2,8 +2,9 @@ function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
   for(var i=0;i<collection_a.length;i++){
     for(var x=0;x<object_b.value.length;x++){
+      var sum=0;
       if(collection_a[i].key==object_b.value[x]){
-        var sum=parselnt(collection_a[i].count/3);
+        sum=parselnt(collection_a[i].count/3);
         collection_a[i].count=collection_a[i].count-sum;
       }
     }
