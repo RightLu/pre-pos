bbbb1f6408a982e74b9c96c05b5520dfa17d486a
renlu <rightlu@outlook.com>
2015-05-17 23:06:39 +0800

    add fifth pre-pos

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,13 +1,14 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
   for(var i=0;i<collection_a.length;i++){
-    for(var x=0;i<object_b.value.length;x++){
-      if(collection_a[i].key==object_b.value[x]){
-        collection_a[i].count--;
-      }
-    }
-  }
-  return collection_a;
+       for(var x=0;x<object_b.value.length;x++){
+         if(collection_a[i].key==object_b.value[x]){
+           collection_a[i].count--;
+         }
+       }
+   }
+   return collection_a;
 }
+
 
 module.exports = create_updated_collection;
