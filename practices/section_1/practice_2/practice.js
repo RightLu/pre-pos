81d771e27b672de30951d213f55d979a53efe078
renlu <rightlu@outlook.com>
2015-05-17 21:55:45 +0800

    add second pre-pos

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+var collect_same=[];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<collection_b[0].length;x++){
+      if(collection_a[i]==collection_b[0][x]){
+        collect_same.push(collection_a[i]);
+      }
+    }
+  }
+  return collect_same;
 }
 
 module.exports = collect_same_elements;
