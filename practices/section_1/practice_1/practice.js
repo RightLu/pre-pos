906722615d6b01a7e1fff0e49680c342cf30be25
renlu <rightlu@outlook.com>
2015-05-17 21:11:34 +0800

    add first pre-pos

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var collect_same=[];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<collection_b.length;x++){
+      if( collection_a[i]==collection_b[x]){
+        collect_same.push(collection_b[x]);
+      }
+    }
+  }
+   return  collect_same;
 }
 
 module.exports = collect_same_elements;
