84158f0323bc0a28a27c63922de2f0f15da4c14a
renlu <rightlu@outlook.com>
2015-05-17 22:06:10 +0800

    add third pre-pos

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var collect_same=[];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<object_b.value.length;x++){
+      if(collection_a[i]==object_b.value[x]){
+        collect_same.push(collection_a[i]);
+      }
+    }
+  }
+  return collection;
 }
-
 module.exports = collect_same_elements;
