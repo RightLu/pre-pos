42a34d635217821b32815b697704de431d6f15d1
renlu <rightlu@outlook.com>
2015-05-17 22:38:10 +0800

    add fourth pre-pos

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,6 +1,6 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
-  var collect_same[];
+  var collect_same=[];
   for(var i=0;i<collection_a.length;i++){
     for(var x=0;x<object_b.value.length;x++){
       if(collection_a[i].key==object_b.value[x]){
