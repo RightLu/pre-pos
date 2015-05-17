750d725d633e619100167d996ea67eb0075d2550
renlu <rightlu@outlook.com>
2015-05-17 22:26:45 +0800

    add fourth pre-pos

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var collect_same[];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<object_b.value.length;x++){
+      if(collection_a[i].key==object_b.value[x]){
+        collec_same.push(object_b.value);
+      }
+    }
+  }
+  return collect_same;
 }
 
 module.exports = collect_same_elements;
