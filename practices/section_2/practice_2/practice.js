8c6cf5ccb204aa523b3e6583ac635f8f3a31e8e3
renlu <rightlu@outlook.com>
2015-05-18 16:32:41 +0800

    add eighth pre-pos

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -3,21 +3,21 @@ function count_same_elements(collection) {
     var array = [];
 
     for(var i = 0; i < collection.length; i++) {
-        var exist = flase;
-        var char = collection[i].length > 2 ? collection[i].substring(0,1) : collection[i];
-        var summary = collection[i].length > 2 ? parsenInt(collection[i].substring(2)) : 1;
+        var exist = false;
+        var key = collection[i].length > 2 ? collection[i].substring(0,1) : collection[i];
+        var count = collection[i].length > 2 ? parseInt(collection[i].substring(2)) : 1;
 
         for(var x = 0; x < array.length; x++) {
-            if(array[x].name === char) {
-                array[i].summary = array[x].summary + summary;
+            if(array[x].key === key) {
+                array[x].count = array[x].count + count;
                 exist = true;
                 break;
             }
         }
         if(!exist) {
             var temp = {};
-            temp.name = char;
-            temp.summary =summary;
+            temp.key = key;
+            temp.count = count;
             array.push(temp);
         }
     }
