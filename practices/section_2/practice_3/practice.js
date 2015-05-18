b5ce2bccb5051b9c4a3d3b239963f246ca0aa5f5
renlu <rightlu@outlook.com>
2015-05-18 16:37:39 +0800

    add ninth pre-pos

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,27 +1,27 @@
 function count_same_elements(collection) {
   //在这里写入代码
-  var array = [];
+    var array = [];
 
-  for(var i = 0; i < collection.length; i++) {
-      var exist = flase;
-      var char = collection[i].length > 2 ? collection[i].substring(0,1) : collection[i];
-      var summary = collection[i].length > 2 ? parsenInt(collection[i].substring(2)) : 1;
+    for(var i = 0; i < collection.length; i++) {
+        var exist = false;
+        var char = collection[i].length > 2 ? collection[i].substring(0,1) : collection[i];
+        var summary = collection[i].length > 2 ? parseInt(collection[i].substring(2)) : 1;
 
-      for(var x = 0; x < array.length; x++) {
-          if(array[x].name === char) {
-              array[i].summary = array[x].summary + summary;
-              exist = true;
-              break;
-          }
-      }
-      if(!exist) {
-          var temp = {};
-          temp.name = char;
-          temp.summary = summary;
-          array.push(temp);
-      }
-  }
-  return array;
+        for(var x = 0; x < array.length; x++) {
+            if(array[x].name === char) {
+                array[x].summary = array[x].summary + summary;
+                exist = true;
+                break;
+            }
+        }
+        if(!exist) {
+            var temp = {};
+            temp.name = char;
+            temp.summary = summary;
+            array.push(temp);
+        }
+    }
+    return array;
 }
 
 module.exports = count_same_elements;
