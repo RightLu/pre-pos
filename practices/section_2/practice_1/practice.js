3f02b5fb1af784e394d62b2a91a919150e8391b8
renlu <rightlu@outlook.com>
2015-05-18 15:04:04 +0800

    add seventh pre-pos

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,23 @@
 function count_same_elements(collection) {
   //在这里写入代码
+    var array = [];
+
+    for(var i = 0; i < collection.length; i++){
+        var exist = false;
+        for(var x = 0; x < array.length; x++){
+            if(array[x].key == collection[i]){
+                array[x].count ++;
+                exist = true;
+            }
+        }
+        if(!exist){
+            var temp = {};
+            temp.key = collection[i];
+            temp.count = 1;
+            array.push(temp);
+        }
+    }
+    return array;
 }
 
 module.exports = count_same_elements;
